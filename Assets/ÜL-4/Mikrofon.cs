8aca1da951cf7a5f1f64fc8443a528e7dab270e1
Õnnela Link <146323564+onnelaa@users.noreply.github.com>
2025-04-06 19:03:02 +0300

    update mikrofon

--- Assets/ÜL-4/Mikrofon.cs
+++ Assets/ÜL-4/Mikrofon.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Mikrofon : MonoBehaviour
 {
@@ -11,14 +12,62 @@ public class Mikrofon : MonoBehaviour
         microphonePanel.SetActive(true);
     }
 
+    public AudioSource audioSource;
+    private bool isRecording = false;
+    private string selectedDevice;
+
+    [Header("UI Settings")]
+    public Button toggleButton;
+    public Text statusText;
+
     void Start()
     {
         
+        // Kontrolli mikrofone
+        if (Microphone.devices.Length == 0)
+        {
+            statusText.text = "Mikrofoni ei leitud!";
+            toggleButton.interactable = false;
+            return;
+        }
+
+        selectedDevice = Microphone.devices[0];
+        statusText.text = "Mikrofon valmis";
+        toggleButton.onClick.AddListener(ToggleMicrophone);
     }
 
-    // Update is called once per frame
-    void Update()
+    void ToggleMicrophone()
     {
-        
+        if (isRecording)
+        {
+            // Lõpeta salvestamine
+            Microphone.End(selectedDevice);
+            audioSource.Stop();
+            statusText.text = "Mikrofon välja lülitatud";
+        }
+        else
+        {
+            // Alusta salvestamist
+            audioSource.clip = Microphone.Start(selectedDevice, true, 10, 44100);
+            
+            // Oota kuni mikrofon käivitub
+            while (!(Microphone.GetPosition(selectedDevice) > 0)) { }
+            
+            audioSource.loop = true;
+            audioSource.Play();
+            statusText.text = "Mikrofon sisse lülitatud";
+        }
+
+        isRecording = !isRecording;
+    }
+
+    void OnDestroy()
+    {
+        if (isRecording)
+        {
+            Microphone.End(selectedDevice);
+        }
     }
 }
+
+
